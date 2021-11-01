bfd4bd99796c46abdd059df1a226f850d6c9447e
l4kyxa <l4kyxa@gmail.com>
2021-11-01 19:24:04 +0100

    rolunk.php bemutatkozás megírása

--- Front-End/rolunk.php
+++ Front-End/rolunk.php
@@ -43,7 +43,72 @@
                 <h3>BEMUTATKOZÁS</h3>
             </div>
         </div>
+        <div class="content">
+            <h3>Szerencsés helyzetben vagyok, hiszen két hónapja vehettem át a nagyszüleim által indított rövidáru
+                és gomb kereskedést, melyről elmondhatom, hogy apáról-fiúra szállt, tehát családi vállalkozás.
+                Nagy lehetőség van benne, hiszen Budapesten közlekedési szempontból is kiváló helyen fekszik.
+                Az árukészlet adott volt, amikor átvettem a vállalkozást.</h3>
 
+            <hr>
+
+            <div class="content">
+                <h3>A jelenlegi árukészlet legfőbb termékei a gumi, cérna, tű, szalag, cipzár, cipőfűző, gomb, melyet
+                    folyamatosan igyekszem bővíteni fajtája, mérete, színe alapján a vásárlók kérésének,
+                    kívánságának megfelelően, illetve más üzletek termékeit figyelemmel kísérve.</h3>
+            </div>
+
+            <hr>
+
+            <div class="content">
+                <h3>Az üzlet népszerű, kedvelt elsősorban az idősebb korosztály körében,
+                    valamint a rövidáru termékeket nagyobb mennyiségben vásárló varró,
+                    illetve hasonló szakmában dolgozók számára. Jelenleg az üzletnek nincs saját weboldala.</h3>
+            </div>
+
+            <hr>
+
+            <div class="content">
+                <h3>Manapság a legtöbb vállalkozás számára óriási előnyt jelent,
+                    ha rendelkezik egy saját honlappal, hiszen egy cég ilyen irányú hirdetése,
+                    illetve egy jó termékkereső rendszerrel rendelkező,
+                    megfelelően kivitelezett internetes vásárlás nagy mértékben fellendítheti
+                    egy üzlet hírnevét és forgalmát. Úgy gondolom, most jött el az ideje,
+                    hogy a cégnek saját weblapja is legyen.</h3>
+            </div>
+
+            <hr>
+
+            <div class="content">
+                <h3> Célom,
+                    hogy vásárlóink naprakész információkkal rendelkezzenek a termékkészlettel kapcsolatban.
+                    Természetesen az árukészletet különböző alkategóriák szerint,
+                    mint pl. cipzár: 16 cm széles, fehér színű, fém fogazatú alkategória alapján adatbázisba kellene
+                    szervezni egy jól működő keresőrendszerrel. </h3>
+            </div>
+
+            <hr>
+
+            <div class="content">
+                <h3>Fontos számomra az árukészlet folyamatos bővítése,
+                    figyelembe véve a vásárlók kérését, kívánságát, igényeit. Szeretném továbbá a fiatalabb korosztályt
+                    is megnyerni egyrészt
+                    az aktuális divat, trendi állandó nyomon követésével, másrészt azzal,
+                    hogy egy kreatív hobbi aloldalt is ki lehet alakítani, amelyen a honlap felhasználói megoszthatják
+                    egymással új ötleteiket,
+                    elképzeléseiket, pl. különböző anyagokból milyen mintákat, praktikus megoldásokat tudnak létrehozni.
+                </h3>
+            </div>
+
+            <hr>
+
+            <div class="content">
+                <h3> Elvárt, hogy a weboldal könnyen üzemeltethető legyen.
+                    A projekt elkészítése …… nyelveken történik.
+                    Az online megjelenés lehetőleg a PC/Laptopon kívül mobiltelefonon és tableten is működjön,
+                    reszponzív felülettel.</h3>
+            </div>
+        </div>
+    </div>
        
 <!---->
 <div id="footer">
