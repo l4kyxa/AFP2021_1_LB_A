b3fc18fc2c8a9262e831f248f3770e6c5fc3e3a1
l4kyxa <l4kyxa@gmail.com>
2021-11-03 10:37:59 +0100

    style.css kiegészítése rolunk php fekete szövegszin fehérre váltása

--- Front-End/rolunk.php
+++ Front-End/rolunk.php
@@ -41,73 +41,74 @@
     <div id="box_1">
         <div class="btncontainer">
             <div class="btnvertical-center">
-                <h3>BEMUTATKOZÁS</h3>
+                <h2>BEMUTATKOZÁS</h2>
             </div>
         </div>
 
         <div class="content">
-            <h3>Szerencsés helyzetben vagyok, hiszen két hónapja vehettem át a nagyszüleim által indított rövidáru
+
+            <p>Szerencsés helyzetben vagyok, hiszen két hónapja vehettem át a nagyszüleim által indított rövidáru
                 és gomb kereskedést, melyről elmondhatom, hogy apáról-fiúra szállt, tehát családi vállalkozás.
                 Nagy lehetőség van benne, hiszen Budapesten közlekedési szempontból is kiváló helyen fekszik.
-                Az árukészlet adott volt, amikor átvettem a vállalkozást.</h3>
-
+                Az árukészlet adott volt, amikor átvettem a vállalkozást.</p>
+        </div>
             <hr>
 
             <div class="content">
-                <h3>A jelenlegi árukészlet legfőbb termékei a gumi, cérna, tű, szalag, cipzár, cipőfűző, gomb, melyet
+                <p>A jelenlegi árukészlet legfőbb termékei a gumi, cérna, tű, szalag, cipzár, cipőfűző, gomb, melyet
                     folyamatosan igyekszem bővíteni fajtája, mérete, színe alapján a vásárlók kérésének,
-                    kívánságának megfelelően, illetve más üzletek termékeit figyelemmel kísérve.</h3>
+                    kívánságának megfelelően, illetve más üzletek termékeit figyelemmel kísérve.</p>
             </div>
 
             <hr>
 
             <div class="content">
-                <h3>Az üzlet népszerű, kedvelt elsősorban az idősebb korosztály körében,
+                <p>Az üzlet népszerű, kedvelt elsősorban az idősebb korosztály körében,
                     valamint a rövidáru termékeket nagyobb mennyiségben vásárló varró,
-                    illetve hasonló szakmában dolgozók számára. Jelenleg az üzletnek nincs saját weboldala.</h3>
+                    illetve hasonló szakmában dolgozók számára. Jelenleg az üzletnek nincs saját weboldala.</p>
             </div>
 
             <hr>
 
             <div class="content">
-                <h3>Manapság a legtöbb vállalkozás számára óriási előnyt jelent,
+                <p>Manapság a legtöbb vállalkozás számára óriási előnyt jelent,
                     ha rendelkezik egy saját honlappal, hiszen egy cég ilyen irányú hirdetése,
                     illetve egy jó termékkereső rendszerrel rendelkező,
                     megfelelően kivitelezett internetes vásárlás nagy mértékben fellendítheti
                     egy üzlet hírnevét és forgalmát. Úgy gondolom, most jött el az ideje,
-                    hogy a cégnek saját weblapja is legyen.</h3>
+                    hogy a cégnek saját weblapja is legyen.</p>
             </div>
 
             <hr>
 
             <div class="content">
-                <h3> Célom,
+                <p> Célom,
                     hogy vásárlóink naprakész információkkal rendelkezzenek a termékkészlettel kapcsolatban.
                     Természetesen az árukészletet különböző alkategóriák szerint,
                     mint pl. cipzár: 16 cm széles, fehér színű, fém fogazatú alkategória alapján adatbázisba kellene
-                    szervezni egy jól működő keresőrendszerrel. </h3>
+                    szervezni egy jól működő keresőrendszerrel. </p>
             </div>
 
             <hr>
 
             <div class="content">
-                <h3>Fontos számomra az árukészlet folyamatos bővítése,
+                <p>Fontos számomra az árukészlet folyamatos bővítése,
                     figyelembe véve a vásárlók kérését, kívánságát, igényeit. Szeretném továbbá a fiatalabb korosztályt
                     is megnyerni egyrészt
                     az aktuális divat, trendi állandó nyomon követésével, másrészt azzal,
                     hogy egy kreatív hobbi aloldalt is ki lehet alakítani, amelyen a honlap felhasználói megoszthatják
                     egymással új ötleteiket,
                     elképzeléseiket, pl. különböző anyagokból milyen mintákat, praktikus megoldásokat tudnak létrehozni.
-                </h3>
+                </p>
             </div>
 
             <hr>
 
             <div class="content">
-                <h3> Elvárt, hogy a weboldal könnyen üzemeltethető legyen.
+                <p> Elvárt, hogy a weboldal könnyen üzemeltethető legyen.
                     A projekt elkészítése …… nyelveken történik.
                     Az online megjelenés lehetőleg a PC/Laptopon kívül mobiltelefonon és tableten is működjön,
-                    reszponzív felülettel.</h3>
+                    reszponzív felülettel.</p>
             </div>
         </div>
     </div>
