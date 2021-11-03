3f549a5217e85ae01202e5f96c9f93bbbbfe4e95
l4kyxa <l4kyxa@gmail.com>
2021-11-03 17:37:37 +0100

    Lábléc betűtípus és méret változtatása

--- Front-End/uzenofal.php
+++ Front-End/uzenofal.php
@@ -97,27 +97,26 @@
                 <a href="index.php" target="_blank" rel="noopener"> <i class="fa fa-globe"
                                                                        style="font-size:60px;color:white;text-shadow:2px 2px 4px #000000;"></i></a>
                 <h3>Web</h3>
-                <h2 style="font-size:15px;">www.gombrovidaru.hu</h2>
+                <h4>www.gombrovidaru.hu</h4>
             </div>
 
             <div class="flex-item">
                 <a><i class="fa fa-phone" style="font-size:60px;color:white;text-shadow:2px 2px 4px #000000;"></i></a>
                 <h3>Telefon</h3>
-                <h2 style="font-size:15px;">+36-30 /... - .. - ..</h2>
+                <h4 >+36-30 /... - .. - ..</h4>
             </div>
 
             <div class="flex-item">
                 <a href="https://www.google.com/maps/place/Budapest,+Istv%C3%A1n+%C3%BAt+12,+1041/@47.5610963,19.087483,17z/data=!3m1!4b1!4m5!3m4!1s0x4741da36e2c14bf1:0xeb4c96c4aabc8193!8m2!3d47.5610963!4d19.0896717"
-                   target="_blank" rel="noopener"><i class="fa  fa-home"
-                                                     style="font-size:60px;color:white;text-shadow:2px 2px 4px #000000;"></i></a>
+                   target="_blank" rel="noopener"><i class="fa  fa-home" style="font-size:60px;color:white;text-shadow:2px 2px 4px #000000;"></i></a>
                 <h3>Székhely</h3>
-                <h2 style="font-size:15px;">H. 1046. Budapest, István út 12.</h2>
+                <h4>H. 1046. Budapest, István út 12.<h4>
             </div>
         </div>
         <hr>
         <div class="btncontainer">
             <div class="btnvertical-center">
-                <p>• ©2021 Interlude • All Rights Reserved • Adapted by ... •</p>
+                <h3>• ©2021 Interlude • All Rights Reserved • Adapted by gombrovidaru •</h3>
             </div>
         </div>
     </div>
