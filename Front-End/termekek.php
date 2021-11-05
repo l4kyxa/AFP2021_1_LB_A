9599ad44cbcc90e031075f5968fa05cea2e5c0d9
l4kyxa <l4kyxa@gmail.com>
2021-11-05 20:45:17 +0100

    termekek.php átszervezése

--- Front-End/termekek.php
+++ Front-End/termekek.php
@@ -16,14 +16,7 @@
         <a class="logo" href="kezdolap.php"><i class="fa fa-dot-circle-o" aria-hidden="true"></i></a>
         <a href="kezdolap.php"><i class="fa fa-fw fa-home"></i> Kezdőlap</a>
         <a href="rolunk.php"><i class="fa fa-fw fa-user-circle-o"></i> RÓLUNK</a>
-        <div class="dropdown">
-            <button class="dropbtn"><i class="fa fa-fw fa-hand-o-down"></i> TERMÉKEK</button>
-            <div class="dropdown-content">
-                <a class="active" href="rovidaru.php"><i class="fa fa-fw fa-hand-o-right"></i> Rövidáru</a>
-                <a href="gombok.php"><i class="fa fa-fw fa-hand-o-right"></i> Gomb</a>
-                <a href="akciokkifutok.php"><i class="fa fa-fw fa-hand-o-right"></i> Akciós / kifutó termékek</a>
-            </div>
-        </div>
+        <a class="active" href="termekek.php"><i class="fa fa-fw fa-hand-o-down"></i> TERMÉKEK</a>
         <a href="uzenofal.php"><i class="fa fa-fw fa-commenting-o"></i> ÜZENŐFAL</a>
         <a href="kapcsolat.php"><i class="fa fa-fw fa-vcard-o"></i> KAPCSOLAT</a>
         <a href="bejelentkezes.php"><i class="fa fa-fw fa-sign-in"></i> BEJELENTKEZÉS</a>
