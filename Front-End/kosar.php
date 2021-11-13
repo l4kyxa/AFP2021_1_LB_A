d68ce588de5bad88fbba2135f9f78d7decce95cf
Csathó Zsolt <csatho.zsolt@gmail.com>
2021-11-13 07:19:28 +0100

    Konfliktusok kezelése

--- Front-End/kosar.php
+++ Front-End/kosar.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html>
-<title>www.gombrovidaru.hu</title>
-<meta charset="UTF-8">
-<link href="images/favicon.png" rel="icon">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-<!--  <meta name="viewport" content="width=device-width, initial-scale=1"> -->
-<link href="css/style.css" rel="stylesheet" type="text/css"/>
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inconsolata">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 <body>
 <!---->
