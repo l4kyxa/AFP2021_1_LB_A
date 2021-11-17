6eabf5c8d8a1143d397a7837bc774206179cb9d5
László Suhajda <lszlshjd687@gmail.com>
2021-11-17 12:40:30 +0100

    Regisztráció ver.2 (scriptek, funkció/adatbáziskezelő vázlat)

--- Front-End/php/00_regisztracio.php
+++ Front-End/php/00_regisztracio.php
@@ -2,24 +2,45 @@
 
 if (isset($_POST["submit"])) 
 {
-	echo "Működik";
+	$username = $_POST["felhasznaloinev"];
+	$email = $_POST["email"];
+	$pwd = $_POST["jelszo"];
+	$pwdrepeat = $_POST["jelszo_ujra"];
+
+	require_once 'dbhandler.php';
+	require_once 'functions.php';
+
+	if (emptyInputSignup($username, $email, $pwd, $pwdrepeat) !== false)
+	{
+		header ("location: ../regisztracio.php?error=emptyinput");
+		exit();
+	}
+	if (invalidUid($felhasznaloinev) !== false)
+	{
+		header ("location: ../regisztracio.php?error=invaliduid");
+		exit();
+	}
+	if (invalidEmail($email) !== false)
+	{
+		header ("location: ../regisztracio.php?error=invalidemail");
+		exit();
+	}
+	if (pwdMatch($pwd, $pwdrepeat) !== false)
+	{
+		header ("location: ../regisztracio.php?error=passwordsdontmatch");
+		exit();
+	}
+	if (uidExists($conn, $username) !== false)
+	{
+		header ("location: ../regisztracio.php?error=usernametaken");
+		exit();
+	}
+
+	createUser($conn, $username, $email, $pwd);
+
 }
-else {
-	header ("location: ../signup.php");
-
-
->
-/*<html>
-<body>
-
-Back-End felé küldött adatok<br><br>
-Felhasználói név:<?php echo $_POST["felhasznaloinev"]; ?><br>
-Email: <?php echo $_POST["email"]; ?><br>
-Jelszó: <?php echo $_POST["jelszo"]; ?><br>
-Jelszó újra: <?php echo $_POST["jelszo_ujra"]; ?><br>
-<br><br>
-10 mp múlva átirányítjuk a kezdőlapra
-<?php header( 'Refresh: 10; URL=..\index.php');?>
-
-</body>
-</html>*/
+else 
+{
+	header ("location: ../regisztracio.php");
+	exit();
+}
