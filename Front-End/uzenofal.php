5d16be4a1c50c84706ee1b2d009d70316b9fbc99
l4kyxa <72296746+l4kyxa@users.noreply.github.com>
2021-11-24 22:33:43 +0100

    footer.php csatolása uzenofal.php-hoz

--- Front-End/uzenofal.php
+++ Front-End/uzenofal.php
@@ -84,39 +84,7 @@
     </div>
 </div>
 <!---->
-<div id="footer">
-    <div id="box_1">
-        <h2> Pál és Vásárhelyi BT.</h2>
-        <div class="flex-container">
-
-            <div class="flex-item">
-                <a href="index.php" target="_blank" rel="noopener"> <i class="fa fa-globe"
-                                                                       style="font-size:60px;color:white;text-shadow:2px 2px 4px #000000;"></i></a>
-                <h3>Web</h3>
-                <h4>www.gombrovidaru.hu</h4>
-            </div>
-
-            <div class="flex-item">
-                <a><i class="fa fa-phone" style="font-size:60px;color:white;text-shadow:2px 2px 4px #000000;"></i></a>
-                <h3>Telefon</h3>
-                <h4 >+36-30 /123 - 45 - 67</h4>
-            </div>
-
-            <div class="flex-item">
-                <a href="https://www.google.com/maps/place/Budapest,+Istv%C3%A1n+%C3%BAt+12,+1041/@47.5610963,19.087483,17z/data=!3m1!4b1!4m5!3m4!1s0x4741da36e2c14bf1:0xeb4c96c4aabc8193!8m2!3d47.5610963!4d19.0896717"
-                   target="_blank" rel="noopener"><i class="fa  fa-home" style="font-size:60px;color:white;text-shadow:2px 2px 4px #000000;"></i></a>
-                <h3>Székhely</h3>
-                <h4>H. 1046. Budapest, István út 12.<h4>
-            </div>
-        </div>
-        <hr>
-        <div class="btncontainer">
-            <div class="btnvertical-center">
-                <h3>• ©2021 Interlude • All Rights Reserved • Adapted by gombrovidaru •</h3>
-            </div>
-        </div>
-    </div>
-</div>
+<?php include "./footer.php" ?>
 <!---->
 <button onclick="topFunction()" id="laptetejeregomb" title="vissza a lap tetejére"><i class="fa fa-arrow-up" aria-hidden="true"></i></button>
 <!-- -->
