1a21c90a93f14fb0bc8f8000fd2f9821e8ee03bf
lszlshjd687@gmail.com <lszlshjd687@gmail.com>
2021-11-29 00:50:09 +0100

    Adatbázishoz való kapcsolódás kijavítása

--- Front-End/php/dbhandler.php
+++ Front-End/php/dbhandler.php
@@ -1,14 +1,14 @@
 <?php
 
-$serverName = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dBName = "teszteles";
+$serverName = "mysql.rackhost.hu";
+$dbUsername = "c22578keret";
+$dbPassword = "c22578keret";
+$dBName = "c22578DB";
 
 $conn = mysqli_connect($serverName, $dbUsername, $dbPassword, $dBName);
 
 if (!$conn) {
 	die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
+echo "";
 ?>
