66afa6401981819da9d584043cdbbef4ee615bcd
lszlshjd687@gmail.com <lszlshjd687@gmail.com>
2021-11-29 00:52:30 +0100

    Üzenőfal back-end fájl megírása, funkciók meghívása, hibakezelés

--- Front-End/php/00_uzenofal.php
+++ Front-End/php/00_uzenofal.php
@@ -1,14 +1,31 @@
-<html>
-<body>
+<?php
+session_start();
+if (isset($_POST["submit3"]))
+{
+    $neve = $_SESSION["useruid"];
+    $email = $_SESSION["useremail"];
+    $uzenet = $_POST["uzenet"];
+    $datum = $_POST["datum"];
 
-Back-End felé küldött adatok<br><br>
-Felhasználói név:<?php echo $_POST["felhasznaloinev"]; ?><br>
-Email: <?php echo $_POST["email"]; ?><br>
-Üzenet: <?php echo $_POST["uzenet"]; ?><br>
+    require_once 'dbhandler.php';
+	require_once 'functions.php';
 
-<br><br>
-10 mp múlva átirányítjuk a kezdőlapra
-<?php header( 'Refresh: 10; URL=..\index.php');?>
+    if (uidWrongUser($conn, $neve) !== true)
+	{
+		header ("location: ../uzenofal.php?error=wrongusername");
+		exit();
+	}
 
-</body>
-</html>
+    if (uidWrongEmail($conn, $email) !== true)
+	{
+		header ("location: ../uzenofal.php?error=wrongemail");
+		exit();
+	}
+
+    setComment($conn, $neve, $email, $uzenet, $datum);
+}
+else
+{
+    header ("location: ../uzenofal.php");
+	exit();
+}
