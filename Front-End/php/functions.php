48e4d46478089d5e8525f9c95d9088cd5d1433dc
lszlshjd687@gmail.com <lszlshjd687@gmail.com>
2021-11-29 00:51:15 +0100

    Üzenőfal használatához szükséges funkciók hozzáadása

--- Front-End/php/functions.php
+++ Front-End/php/functions.php
@@ -52,7 +52,7 @@ function pwdMatch($pwd, $pwdrepeat)
 	}
 	return $result;
 }
-function uidExists($conn, $username, $email)
+function uidExistss($conn, $username, $email)
 {
 	$sql = "SELECT * FROM felhasznalok WHERE Felhasznalonev = ? OR Email = ?;";
 	$stmt = mysqli_stmt_init($conn);
@@ -114,7 +114,7 @@ function emptyInputLogin($username, $pwd)
 
 function loginUser($conn, $username, $pwd)
 {
-	$uidExists = uidExists($conn, $username, $username);
+	$uidExists = uidExistss($conn, $username, $username);
 
 	if ($uidExists === false)
 	{
@@ -135,7 +135,83 @@ function loginUser($conn, $username, $pwd)
 		session_start();
 		$_SESSION["userid"] = $uidExists["Felhasznalo_ID"];
 		$_SESSION["useruid"] = $uidExists["Felhasznalonev"];
+		$_SESSION["useremail"] = $uidExists["Email"];
 		header("location: ../index.php");
 		exit();
 	}
+}
+
+function setComment($conn, $neve, $email, $uzenet, $datum)
+{
+	$sql = "INSERT INTO bejegyzes (Neve, Emailcim, Uzenet, Datum) VALUES (?, ?, ?, ?) ;";
+	$stmt = mysqli_stmt_init($conn);
+	if (!mysqli_stmt_prepare($stmt, $sql))
+	{
+		header ("location: ../uzenofal.php?error=stmtfailed");
+		exit();
+	}
+
+	mysqli_stmt_bind_param($stmt, "ssss", $neve, $email, $uzenet, $datum);
+	mysqli_stmt_execute($stmt);
+	mysqli_stmt_close($stmt);
+	header ("location: ../uzenofal.php?error=none");
+	exit();
+}
+
+function uidWrongUser($conn, $neve)
+{
+	$sql = "SELECT * FROM felhasznalok WHERE Felhasznalonev = ? ;";
+	$stmt = mysqli_stmt_init($conn);
+	if (!mysqli_stmt_prepare($stmt, $sql))
+	{
+		header ("location: ../regisztracio.php?error=stmtfailed");
+		exit();
+	}
+
+	mysqli_stmt_bind_param($stmt, "s", $neve);
+	mysqli_stmt_execute($stmt);
+
+	$resultData = mysqli_stmt_get_result($stmt);
+
+	if ($row = mysqli_fetch_assoc($resultData))
+	{
+		$result = true;
+		return $result;
+	}
+	else
+	{
+		$result = false;
+		return $result;
+	}
+
+	mysqli_stmt_close($stmt);
+}
+
+function uidWrongEmail($conn, $email)
+{
+	$sql = "SELECT * FROM felhasznalok WHERE Email = ?;";
+	$stmt = mysqli_stmt_init($conn);
+	if (!mysqli_stmt_prepare($stmt, $sql))
+	{
+		header ("location: ../regisztracio.php?error=stmtfailed");
+		exit();
+	}
+
+	mysqli_stmt_bind_param($stmt, "s", $email);
+	mysqli_stmt_execute($stmt);
+
+	$resultData = mysqli_stmt_get_result($stmt);
+
+	if ($row = mysqli_fetch_assoc($resultData))
+	{
+		$result = true;
+		return $result;
+	}
+	else
+	{
+		$result = false;
+		return $result;
+	}
+
+	mysqli_stmt_close($stmt);
 }
