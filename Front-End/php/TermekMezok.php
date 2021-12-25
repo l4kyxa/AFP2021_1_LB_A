b854d3376dfedf2150e70baba7e0eac2894fd306
Gabesz79 <pal.gabor1979@gmail.com>
2021-12-25 13:04:04 +0100

    Termékmezők osztály létrehozása (TerrmekMezok.php fájlban)
    Termékmezők osztály létrehozása (TerrmekMezok.php fájlban)

---
+++ Front-End/php/TermekMezok.php
@@ -0,0 +1,15 @@
+<?php
+
+class TermekMezok
+{
+    public $Termekazonosito_ID;
+    public $Aru_ID;
+    public $TermekNev;
+    public $GyartoNev;
+    public $Szin;
+    public $Meret;
+    public $Mennyiseg;
+    public $Megjegyzes;
+    public $Ar;
+    public $Statusz;
+}
