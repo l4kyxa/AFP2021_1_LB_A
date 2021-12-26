3d96337366eea73be8dcd85e4711bcee5da5b2bf
Gabesz79 <pal.gabor1979@gmail.com>
2021-12-26 13:54:41 +0100

    User szerepkör - áruk osztály mezőinek definiálása
    User szerepkör - áruk osztály mezőinek definiálása

---
+++ Front-End/php/user_TermekMezok.php
@@ -0,0 +1,12 @@
+<?php
+
+class user_TermekMezok
+{
+    public $Termekazonosito_ID;
+    public $TermekNev;
+    public $GyartoNev;
+    public $Szin;
+    public $Meret;
+    public $Ar;
+    public $Statusz;
+}
