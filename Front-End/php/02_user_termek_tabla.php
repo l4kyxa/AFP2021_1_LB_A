07b5eeb3bcf45179f860844004ad8491a7a8bbee
Gabesz79 <pal.gabor1979@gmail.com>
2021-12-27 16:07:11 +0100

    02_user_termek_tabla.php fájl létrehozása - Termék - áru keresés alapján való kiíratás táblába
    02_user_termek_tabla.php fájl létrehozása - Termék - áru keresés alapján való kiíratás táblába

---
+++ Front-End/php/02_user_termek_tabla.php
@@ -0,0 +1,19 @@
+<html xmlns="http://www.w3.org/1999/html">
+<body>
+
+<?php require_once '01_user_termek_keres.php'; ?>
+
+<table border="1">
+    <thead>
+    <tr>
+        <th>Termék neve</th>
+        <th>Gyártó</th>
+        <th>Szín</th>
+        <th>Méret</th>
+        <th>Ár</th>
+        <th>Státusz</th>
+    </tr>
+    </thead>
+
+</body>
+</html>
