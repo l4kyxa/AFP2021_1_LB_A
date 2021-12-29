372f0812aaafdda29f4553cf43d8ba3b94ef74d9
Gabesz79 <pal.gabor1979@gmail.com>
2021-12-29 20:20:11 +0100

    03_admin_termeklist_update.php fájlban: 2. áru adatainak  kiíratása beviteli mezőben, amely adatok a későbbiekben módosíthatóvá válnak
    03_admin_termeklist_update.php fájlban: 2. áru adatainak  kiíratása beviteli mezőben, amely adatok a későbbiekben módosíthatóvá válnak

--- Front-End/php/03_admin_termeklist_update.php
+++ Front-End/php/03_admin_termeklist_update.php
@@ -26,6 +26,43 @@ $record = recordFetch() ?>
         <th>Termék készleten (I/N)</th>
     </tr>
     </thead>
+    <tbody>
+    <tr>
+        <td><?= $record['TermekNev']?></td>
+        <td><?= $record['GyartoNev']?></td>
+        <td><?= $record['Szin']?></td>
+        <td><?= $record['Meret']?></td>
+        <td>
+            <form action="03_admin_termeklist_update.php" method="POST" accept-charset="utf-8">
+                <input type ="text" name="Mennymod" value="<?=$record['Mennyiseg']?>" placeholder="mennyiség"/>
+                <input type="hidden" name="aruID" value="<?= $record['Aru_ID'] ?>">
+                <button type="submit" name="Mennymodosit" value="1">Mentés</button>
+            </form>
+        </td>
+        <td>
+            <form action="03_admin_termeklist_update.php" method="POST" accept-charset="utf-8">
+                <input type ="text" name="Megjmod" value="<?=$record['Megjegyzes']?>" placeholder="megjegyzes"/>
+                <input type="hidden" name="aruID" value="<?= $record['Aru_ID'] ?>">
+                <button type="submit" name="Megjmodosit" value="1">Mentés</button>
+            </form>
+        </td>
+        <td>
+            <form action="03_admin_termeklist_update.php?>" method="POST" accept-charset="utf-8">
+                <input type ="text" name="Armod" value="<?=$record['Ar']?>" placeholder="ár"/>
+                <input type="hidden" name="aruID" value="<?= $record['Aru_ID'] ?>">
+                <button type="submit" name="Armodosit" value="1">Mentés</button>
+            </form>
+        </td>
+        <td>
+            <form action="03_admin_termeklist_update.php?>" method="POST" accept-charset="utf-8">
+                <input type="checkbox" placeholder="státusz" name="Statmod" id="Statmod" value="<?=$record['Statusz']?>" required>
+                <input type="hidden" name="aruID" value="<?= $record['Aru_ID'] ?>">
+                <button type="submit" name="Statmodosit" value="1">Mentés</button>
+            </form>
+        </td>
+    </tr>
+    </tbody>
+</table>
 
 
 
