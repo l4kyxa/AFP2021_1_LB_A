a51b5d5cfc7bd464a8c39245d93031a2e665f72c
Gabesz79 <pal.gabor1979@gmail.com>
2021-12-29 19:53:45 +0100

    02_admin_termek_keres_tabla.php fájl létrehozása: - admin szerepkör - keresett termék kiíratása és módosítás, törlés gomb kreálása
    02_admin_termek_keres_tabla.php fájl létrehozása: - admin szerepkör - keresett termék kiíratása és módosítás, törlés gomb kreálása

--- Front-End/php/02_admin_termek_keres_tabla.php
+++ Front-End/php/02_admin_termek_keres_tabla.php
@@ -23,3 +23,37 @@ require_once '01_admin_termek_keres.php';
     </tr>
     </thead>
     <tbody>
+    <?php for ($i = 0; $i < count($atermekek); $i++) { ?>
+        <tr>
+            <td><?= $atermekek[$i] -> TermekNev ?></td>
+            <td><?= $atermekek[$i] -> GyartoNev ?></td>
+            <td><?= $atermekek[$i] -> Szin?></td>
+            <td><?= $atermekek[$i] -> Meret?></td>
+            <td><?= $atermekek[$i] -> Mennyiseg?></td>
+            <td><?= $atermekek[$i] -> Megjegyzes?></td>
+            <td><?= $atermekek[$i] -> Ar?></td>
+            <td><?= $atermekek[$i] -> Statusz?></td>
+            <td>
+                <br>
+                <form name="form1" method="post" action="03_admin_termeklist_update.php">
+                    <input type="hidden" name="aruID" value="<?= $atermekek[$i] -> Aru_ID ?>">
+                    <input type="submit" name="termod" value="Módosítás">
+                </form>
+            </td>
+
+            <td>
+                <br>
+                <form action="03_admin_termek_delete.php" method="POST" onsubmit="return confirm('Biztos vagy benne, hogy törölni szeretnéd a terméket?')">
+                    <input type="hidden" name="aruID" value="<?= $atermekek[$i] -> Aru_ID ?>">
+                    <input type="submit" name="tertor" value="Törlés">
+                </form>
+            </td>
+
+
+        </tr>
+    <?php } ?>
+    </tbody>
+</table>
+
+</body>
+</html>
