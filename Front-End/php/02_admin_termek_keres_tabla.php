aa60e451a53a2d17fff0b16fdf0ced27b2cb9e6e
Gabesz79 <pal.gabor1979@gmail.com>
2021-12-29 19:13:01 +0100

    02_admin_termek_keres_tabla.php fájl létrehozása: - admin szerepkör - keresett termék kiíratása és módosítás, törlés előkészítése
    02_admin_termek_keres_tabla.php fájl létrehozása: - admin szerepkör - keresett termék kiíratása és módosítás, törlés előkészítése

---
+++ Front-End/php/02_admin_termek_keres_tabla.php
@@ -0,0 +1,25 @@
+<?php
+
+
+require_once '01_admin_termek_keres.php';
+
+?>
+
+<html>
+<body>
+
+<table border="1">
+    <thead>
+    <tr>
+        <th>Termék neve</th>
+        <th>Gyártó</th>
+        <th>Szín</th>
+        <th>Méret</th>
+        <th>Mennyiség</th>
+        <th>Megjegyzés</th>
+        <th>Ár</th>
+        <th>Státusz</th>
+        <th colspan="2">Műveletek</th>
+    </tr>
+    </thead>
+    <tbody>
