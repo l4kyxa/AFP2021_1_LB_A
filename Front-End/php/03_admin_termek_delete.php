f536a679450185d168aa00aed64fca823b3d8dd8
Gabesz79 <pal.gabor1979@gmail.com>
2021-12-30 13:13:31 +0100

    03_admin_termek_delete.php fájl létrehozása - Áru törlése - Törlés gombra kattintva: tertor POST-olása a 03_admin_termek_delete.php-ben, sql utasítással delete lefuttatása, kiíratás a törlés eredményéről
    03_admin_termek_delete.php fájl létrehozása - Áru törlése - Törlés gombra kattintva: tertor POST-olása a 03_admin_termek_delete.php-ben, sql utasítással delete lefuttatása, kiíratás a törlés eredményéről

---
+++ Front-End/php/03_admin_termek_delete.php
@@ -0,0 +1,18 @@
+<?php
+if(isset($_POST["tertor"])) {
+    require_once 'dbhandler.php';
+
+    $aID = $_POST['aruID'];
+
+    $sql = "DELETE FROM aruk WHERE Aru_ID = '$aID'";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Record deleted successfully";
+    } else {
+        echo "Error deleting record: " . $conn->error;
+    }
+
+    $conn->close();
+}
+
+?>
